54d3290f46e1bb5208111f17e7ed1e615cc0691c
lucasamonrc <lucasamonrc@gmail.com>
2022-07-23 20:52:44 -0600

    implement infinite scroll

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
+
 import { Header } from './components/Header';
 import { Image } from './components/Image';
-import { getLast10Pictures } from './lib/nasa';
+import { subtractOneDay } from './lib/dates';
+import { get10MorePictures, getLast10Pictures } from './lib/nasa';
+
+const CALLS_LIMIT = 2;
 
 interface Picture {
   title: string;
@@ -11,6 +16,7 @@ interface Picture {
 
 function App() {
   const [pictures, setPictures] = useState<Picture[]>([]);
+  const [counter, setCounter] = useState(0);
 
   useEffect(() => {
     if (pictures.length === 0) {
@@ -20,17 +26,33 @@ function App() {
     }
   }, []);
 
-  console.log(pictures);
+  async function getMore() {
+    const lastDate = pictures.at(-1)?.date;
+    const endDate = subtractOneDay(new Date(lastDate as string));
+
+    const pics = await get10MorePictures(endDate);
+
+    const newPictures = [...pictures, ...pics].sort((a, b) => {
+      if (a.date > b.date) return -1;
+      else return 1;
+    });
+
+    setPictures(newPictures);
+    setCounter(counter + 1);
+  }
 
   return (
     <div>
       <Header />
       <main className="my-8">
-        <h1 className="text-3xl font-bold text-sky-400 mb-4">
-          Todays pictures:
-        </h1>
-
-        <div className="columns-3 gap-8">
+        <InfiniteScroll
+          className="columns-3 gap-8"
+          dataLength={pictures.length}
+          next={getMore}
+          hasMore={pictures.length > 0 && counter < CALLS_LIMIT}
+          loader={<></>}
+          endMessage={<></>}
+        >
           {pictures.map((pic) => (
             <Image
               key={pic.date}
@@ -39,7 +61,7 @@ function App() {
               title={pic.title}
             />
           ))}
-        </div>
+        </InfiniteScroll>
       </main>
     </div>
   );
--- src/lib/nasa.ts
+++ src/lib/nasa.ts
@@ -14,6 +14,24 @@ interface Picture {
   date: string;
 }
 
+export async function get10MorePictures(endDate: Date) {
+  const startDate = subtractTenDays(endDate);
+
+  const end = formatDate(endDate);
+  const start = formatDate(startDate);
+
+  const { data } = await client.get<Picture[]>(
+    `/apod?start_date=${start}&end_date=${end}`
+  );
+
+  data.sort((a, b) => {
+    if (a.date > b.date) return -1;
+    else return 1;
+  });
+
+  return data;
+}
+
 export async function getLast10Pictures() {
   const endDate = subtractOneDay(new Date());
   const startDate = subtractTenDays(endDate);
